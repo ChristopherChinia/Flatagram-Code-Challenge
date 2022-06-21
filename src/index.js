2a267bb70f1bac88c6427b66b6693c6898fc559e
Oscar Omondi <oscaromondiomondi@gmail.com>
2022-06-22 00:51:08 +0300

    push the element to DOM by grabbing it and then appendChild aka the new comment

--- src/index.js
+++ src/index.js
@@ -13,4 +13,6 @@ document.addEventListener('DOMContentLoaded', () => {
 function handleComment(todo) {
     let ul = document.createElement('li')
     ul.textContent = todo
+    document.querySelector('#comments-list').appendChild(ul)
+
 }
