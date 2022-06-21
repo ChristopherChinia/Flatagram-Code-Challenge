9e2162bc14b11a444be5a96b947758e014adbcac
TabithaKoech <tabbyk1718@gmail.com>
2022-06-21 21:26:17 +0300

    grabbed the like button ID and added  an event listener click

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-// write your code here
-document.addEventListener("DOMContentLoaded", () => {
-    const imageContainer = document.querySelector("div.image-container");
+const likeCount=document.getElementById("like-count");
+let likes;
+document.getElementById("like-button").addEventListener("click",()=>{// grab like button
+    likes += 1;//add likes to it
+    renderLikes();
 });
-
-   
