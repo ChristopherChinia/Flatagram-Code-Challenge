249d3e68d88ae1ea4f93eef4910d87dc044add0f
christopherchinia <chinia214@gmail.com>
2022-06-21 20:27:13 -0400

    capturing the image from html file to js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 // write your code here
 // capturing the title from html using id
+let title = document.getElementById("card-title");
+// capturing the image from html using id
 let image = document.getElementById("card-image");
